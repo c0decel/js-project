e0f53323c88edcbc97bf6ebb5c52f6759de73f51
c0decel <124975714+c0decel@users.noreply.github.com>
2023-04-17 00:53:12 -0700

    Add files via upload
    updated for 1.2 exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('hello world')
-let favoritefood = "sushi"
-document.write(favoritefood)
+let pokemonlist = [];
+
+let pokemon_1 = {
+    name: 'snorlax',
+    height: 2.1,
+    type: ['normal']
+};
+
+let pokemon_2 = {
+    name: 'meouth',
+    height: 0.4
+    type: ['normal']
+};
+
+let pokemon_3 = {
+    name: 'kirlia',
+    height: 0.8,
+    type: ['psychic', 'fairy']
+}
