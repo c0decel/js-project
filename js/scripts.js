4ea85c238e9a79545aa1f9973f2b0382933733c2
c0decel <124975714+c0decel@users.noreply.github.com>
2023-04-29 22:46:48 -0700

    Add files via upload
    exercise 1.5 part 1

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,10 @@ let pokemonList = [
     {name: 'rapidash', height: 5.7, type: ['fire horse']}];
 //formats pokemon stats to write
 console.log(pokemonList);
-for (let i = 0; i < pokemonList.length; i++) {
-    let pokemonStat = `<p>${pokemonList[i].name} (height: ${pokemonList[i].height}ft)`;
-//displays after pokemon with height above 3ft
-if (pokemonList[i].height > 3)
-    pokemonStat += ` ${pokemonList[i].name}'s a big pokemon!`;
-//displays all pokemon name and height
-pokemonStat += '</p>';
-document.write(pokemonStat);
-}
-
+pokemonList.forEach(function(pokemon) {
+    console.log(pokemon);
+    document.write(`<p>${pokemon.name} (height: ${pokemon.height}ft)`)
+    if (pokemon.height > 3)
+        document.write(pokemon.name + ' is a tall pokemon!')
+});
+//writes out pokemon stats
