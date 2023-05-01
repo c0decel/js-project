27b41ab84ab79cb130aac5767fd35ec1568ec5a0
c0decel <124975714+c0decel@users.noreply.github.com>
2023-04-30 17:19:30 -0700

    Add files via upload
    exercise 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,46 @@
-let pokemonList = [
-    {name: 'meouth', height: 1.4, type: ['scratch cat']}, 
-    {name: 'snorlax', height: 2.1, type: ['normal']}, 
-    {name: 'rapidash', height: 5.7, type: ['fire horse']}];
-//formats pokemon stats to write
-console.log(pokemonList);
-pokemonList.forEach(function(pokemon) {
-    console.log(pokemon);
-    document.write(`<p>${pokemon.name} (height: ${pokemon.height}ft)`)
+let pokemonRepository = (function() {
+
+    let pokemonList = [
+        {
+            name: 'meouth',
+            height: 1.4,
+            type: ['scratch cat']
+        }, 
+        {
+            name: 'snorlax',
+            height: 2.1,
+            type: ['normal']
+        }, 
+        {
+            name: 'rapidash',
+            height: 5.7,
+            type: ['fire horse']
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
+    }
+    
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 3)
-        document.write(pokemon.name + ' is a tall pokemon!')
-});
-//writes out pokemon stats
+        document.write(pokemon.name + ' at '+ pokemon.height + 'm is a tall pokemon!' + '<br>')
+    else
+        document.write(pokemon.name + ' - '+ pokemon.height + 'm' + '<br>');
+  });
+  
+
+
+
+
